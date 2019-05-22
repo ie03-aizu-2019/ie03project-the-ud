38dc17677f2d791728043eaca662c50a29e6a47e
shake3277 <syageen@gmail.com>
2019-05-22 11:59:47 +0900

    add

--- cpp_code/Phase1/ex4.cpp
+++ cpp_code/Phase1/ex4.cpp
@@ -25,9 +25,95 @@ struct Point{
   }
 };
 
-struct Edge{
+struct Road{
   int b,e;
 };
+
+struct Edge{
+  int to;
+  double cost;
+};
+
+struct Path{
+  double dist;
+  vector<int> route;
+};
+
+Point findIntersection(Point p1,Point p2,Point p3,Point p4);
+typedef pair<int,int> P;
+typedef pair<Point,P> PP;//(頂点,(辺番号1,辺番号2))
+vector<vector<Edge> > makeGraph(int n,int m,vector<Point> point,vector<Road> road,vector<PP> c);
+vector<PP> makeIntersection(int m,vector<Road> road,vector<Point> point);
+typedef pair<double,int> Pdi;//(最短距離、ノード番号)
+Path dijkstra(int s,int t,int sz,vector<vector<Edge> >& graph);
+vector<int> restore(int s,int t,vector<vector<Edge> >& graph,vector<double> dist);
+vector<Path> yensAlgorithm(int s,int t,vector<vector<Edge> >& graph);
+
+int main(){
+  int n,m,p,q;
+  cin>>n>>m>>p>>q;
+  
+  vector<Point> point;
+  vector<Road> road;
+  map<string,int> stov;
+  
+  for(int i=0;i<n;i++){
+    double x,y;
+    cin>>x>>y;
+    Point pt={x,y,to_string(i+1)};
+    stov[pt.name]=point.size();
+    point.push_back(pt);
+  }
+  
+  for(int i=0;i<m;i++){
+    int b,e;
+    cin>>b>>e;
+    b--,e--;//頂点は0-indexedで管理
+    road.push_back({b,e});
+  }
+
+  vector<PP> intersection=makeIntersection(m,road,point);
+
+  //pointに交差点を追加する
+  for(int i=0;i<intersection.size();i++){
+   Point pt = intersection[i].first;
+   pt.name="C"+to_string(i+1);
+   stov[pt.name]=point.size();
+   point.push_back(pt);
+  }
+  
+  vector<vector<Edge> > graph=makeGraph(n,m,point,road,intersection);
+  
+  for(int i=0;i<q;i++){
+    string stmp,ttmp;
+    int s,t,k;
+    cin>>stmp>>ttmp>>k;
+
+    if(stov.count(stmp)==0||stov.count(ttmp)==0){
+	 cout<<"NA"<<endl;
+	 continue;
+    }
+    
+    s=stov[stmp];
+    t=stov[ttmp];
+   
+    Path path=dijkstra(s,t,(int)point.size(),graph);
+    if(path.dist==INF){
+	 cout<<"NA"<<endl;
+	 continue;
+    }
+    
+    cout<<path.dist<<endl;
+    for(int i=0;i<path.route.size();i++){
+	 if(i)cout<<" ";
+	 int pn=path.route[i];
+	 cout<<point[pn].name;
+    }
+    cout<<endl;
+  }
+
+  return 0;
+}
 
 Point findIntersection(Point p1,Point p2,Point p3,Point p4){
   Point intersection = {INF,INF,"no name"};
@@ -66,32 +152,33 @@ Point findIntersection(Point p1,Point p2,Point p3,Point p4){
   return intersection;
 }
 
-typedef pair<double,int> Pdi;//(最短距離、ノード番号)
-vector<double> dijkstra(int start,const vector<vector<int> >& graph,vector<Point> point){
-  vector<double> dist(point.size(),INF);//dist[i]:=stratから頂点iに対する最短距離
-  dist[start]=0;
+
+Path dijkstra(int s,int t,int sz,vector<vector<Edge> >& graph){
+  vector<double> dist(sz,INF);//dist[i]:=stratから頂点iに対する最短距離
+  dist[s]=0;
   priority_queue<Pdi,vector<Pdi>,greater<Pdi> > que;
-  que.push(Pdi(0,start));
+  que.push(Pdi(0,s));
   while(que.size()>0){
     Pdi tmp=que.top();
     que.pop();
     int v=tmp.second;
     if(dist[v]<tmp.first)continue;
     for(int i=0;i<graph[v].size();i++){
-	 int to=graph[v][i];
-	 if(dist[to]>dist[v]+point[v].dist(point[to])){
-	   dist[to]=dist[v]+point[v].dist(point[to]);
+	 int to=graph[v][i].to;
+	 double cost=graph[v][i].cost;
+	 if(dist[to]>dist[v]+cost){
+	   dist[to]=dist[v]+cost;
 	   que.push(Pdi(dist[to],to));
 	 }
     }
   }
-  return dist;
+  if(dist[t]==INF)return {INF,vector<int>()};
+  Path path={dist[t],restore(s,t,graph,dist)};
+  return path;
 }
 
-typedef pair<int,int> P;
-typedef pair<Point,P> PP;//(頂点,(辺番号1,辺番号2))
-vector<vector<int> > makeGraph(int n,int m,vector<Point> point,vector<Edge> road,vector<PP> c){
-  vector<vector<int> > graph(N);
+vector<vector<Edge> > makeGraph(int n,int m,vector<Point> point,vector<Road> road,vector<PP> c){
+  vector<vector<Edge> > graph(N);
 
   //交差点をx軸の負の方向から見ていき線分を分割する
   for(int i=0;i<c.size();i++){
@@ -104,13 +191,15 @@ vector<vector<int> > makeGraph(int n,int m,vector<Point> point,vector<Edge> road
 
     if(p1<p2){
 	 //交差点の乗った線分に対し左(下)側の端点と交差点をグラフに追加
-	 graph[np1].push_back(n+i);
-	 graph[n+i].push_back(np1);
+	 double cost=p1.dist(point[n+i]);
+	 graph[np1].push_back({n+i,cost});
+	 graph[n+i].push_back({np1,cost});
 	 //左(下)側の端点を交差点に更新
 	 road[edgeNum1].b=n+i;	 
     }else{
-	 graph[np2].push_back(n+i);
-	 graph[n+i].push_back(np2);
+	 double cost=p2.dist(point[n+i]);
+	 graph[np2].push_back({n+i,cost});
+	 graph[n+i].push_back({np2,cost});
 	 road[edgeNum1].e=n+i;	 
     }
 
@@ -120,24 +209,28 @@ vector<vector<int> > makeGraph(int n,int m,vector<Point> point,vector<Edge> road
     p2=point[np2];
 
     if(p1<p2){
-	 graph[np1].push_back(n+i);
-	 graph[n+i].push_back(np1);
+	 double cost=p1.dist(point[n+i]);
+	 graph[np1].push_back({n+i,cost});
+	 graph[n+i].push_back({np1,cost});
 	 road[edgeNum2].b=n+i;	 
     }else{
-	 graph[np2].push_back(n+i);
-	 graph[n+i].push_back(np2);
+	 double cost=p2.dist(point[n+i]);
+	 graph[np2].push_back({n+i,cost});
+	 graph[n+i].push_back({np2,cost});
 	 road[edgeNum2].e=n+i;	 
     }   
   }
   //残った線分をグラフに追加
   for(int i=0;i<m;i++){
-    graph[road[i].b].push_back(road[i].e);
-    graph[road[i].e].push_back(road[i].b);
+    int pn1=road[i].b,pn2=road[i].e;
+    double cost=point[pn1].dist(point[pn2]);
+    graph[pn1].push_back({pn2,cost});
+    graph[pn2].push_back({pn1,cost});
   }
   return graph;
 }
 
-vector<PP> makeIntersection(int m,vector<Edge> road,vector<Point> point){
+vector<PP> makeIntersection(int m,vector<Road> road,vector<Point> point){
   vector<PP> intersection;
   for(int i=0;i<m;i++){
     for(int j=i+1;j<m;j++){
@@ -155,7 +248,7 @@ vector<PP> makeIntersection(int m,vector<Edge> road,vector<Point> point){
 }
 
 //最短経路の頂点番号を返す関数
-vector<int> restore(int s,int t,const vector<vector<int> >& graph,vector<Point> point,vector<double> dist){
+vector<int> restore(int s,int t,vector<vector<Edge> >& graph,vector<double> dist){
   vector<int> route;
   vector<vector<int> > graph2(N);//s-t間の最短経路のみを残した有向グラフ
   queue<int> que;
@@ -169,9 +262,10 @@ vector<int> restore(int s,int t,const vector<vector<int> >& graph,vector<Point> 
     if(used[v])continue;
     used[v]=1;
     for(int i=0;i<graph[v].size();i++){
-	 int to=graph[v][i];
+	 int to=graph[v][i].to;
+	 double cost=graph[v][i].cost;
 	 if(dist[to]>dist[v])continue;
-	 if(abs(dist[v]-dist[to]-point[v].dist(point[to]))<EPS){
+	 if(abs(dist[v]-dist[to]-cost)<EPS){
 	   que.push(to);
 	   graph2[to].push_back(v);
 	 }
@@ -190,71 +284,4 @@ vector<int> restore(int s,int t,const vector<vector<int> >& graph,vector<Point> 
     node=mi;
   }
   return route;
-}
-
-int main(){
-  int n,m,p,q;
-  cin>>n>>m>>p>>q;
-  
-  vector<Point> point;
-  vector<Edge> road;
-  map<string,int> stov;
-  
-  for(int i=0;i<n;i++){
-    double x,y;
-    cin>>x>>y;
-    Point pt={x,y,to_string(i+1)};
-    stov[pt.name]=point.size();
-    point.push_back(pt);
-  }
-  
-  for(int i=0;i<m;i++){
-    int b,e;
-    cin>>b>>e;
-    b--,e--;//頂点は0-indexedで管理
-    road.push_back({b,e});
-  }
-
-  vector<PP> intersection=makeIntersection(m,road,point);
-
-  //pointに交差点を追加する
-  for(int i=0;i<intersection.size();i++){
-   Point pt = intersection[i].first;
-   pt.name="C"+to_string(i+1);
-   stov[pt.name]=point.size();
-   point.push_back(pt);
-  }
-  
-  vector<vector<int> > graph=makeGraph(n,m,point,road,intersection);
-  
-  for(int i=0;i<q;i++){
-    string stmp,ttmp;
-    int s,t,k;
-    cin>>stmp>>ttmp>>k;
-
-    if(stov.count(stmp)==0||stov.count(ttmp)==0){
-	 cout<<"NA"<<endl;
-	 continue;
-    }
-    
-    s=stov[stmp];
-    t=stov[ttmp];
-
-    vector<double> dist=dijkstra(s,graph,point);
-    if(dist[t]==INF){
-	 cout<<"NA"<<endl;
-	 continue;
-    }
-    
-    cout<<dist[t]<<endl;
-
-    vector<int> route=restore(s,t,graph,point,dist);
-    for(int i=0;i<route.size();i++){
-	 if(i)cout<<" ";	 
-	 cout<<point[route[i]].name;
-    }
-    cout<<endl;
-  }
-
-  return 0;
 }
