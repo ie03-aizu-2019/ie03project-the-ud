65c3f74a72adf9447159dc533f1ed1e676289fb8
s1250198 <49445593+s1250198@users.noreply.github.com>
2019-06-18 21:32:33 +0900

    Add files via upload

--- cpp_code/Phase1/P3_test.c
+++ cpp_code/Phase1/P3_test.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define N 1000
+#define M 500
+#define XY 10000
 
 int main(){
+// プログラム実行ごとに異なるパターンが使える
+  unsigned int now = (unsigned int)time( 0 );
+  srand(now);
+
   int n,m,p=0,q=rand()%100; //初期条件, 2<=n<=1000, 1<=M<=500, p=0, 0<=q<=100
   int x,y; // 0≦x,y≦10000,
   int a,b; // a,bはn以下のランダム
   int c,d,e; //Cを含んだランダム
-  srand(1);
+  // プログラム実行ごとに異なるパターンが使える
 
 
-  n=rand()%98+2;
-  m=rand()%50+1;
+
+  n=rand()%(N-1)+2; //999+2
+  m=rand()%M+1; //500+1
   printf("%d %d %d %d\n",n,m,p,q);
   for(int i=0;i<n;i++){
-    x=rand()%10000;
-    y=rand()%10000;
+    x=rand()%XY;
+    y=rand()%XY;
     printf("%d %d\n",x,y);
   }
   //return 0;
 
 
   for(int i=0;i<m;i++){
-    a=rand()%n;
-    b=rand()%n;
+    a=rand()%(n-1)+1;
+    b=rand()%(n-1)+1;
     printf("%d %d\n",a,b);
   }
 
 
   for(int i=0;i<q;i++){
-    c=rand()%n;
-    d=rand()%n;
+    c=rand()%(n-1)+1;
+    d=rand()%(n-1)+1;
     e=1;
 
     printf("%d %d %d\n",c,d,e);
   }
+  return 0;
 }
